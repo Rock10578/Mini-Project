50679762abb22a6a89b18c9eb344a3016534ec1d
Rock10578 <84198247+Rock10578@users.noreply.github.com>
2022-04-27 23:35:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var student={
+    name: "Shreyansh Patwa",
+    age: 20,
+    city: "Ghaziabad"
+}
+console.log(student);
+function sum(){
+    var a=10, b=20
+    console.log(a+b)
+}
+sum()
+function data(name){
+    console.log("Hi",name)
+}
+data("Shreyansh Patwa")
+//document.write('Hello World!')
